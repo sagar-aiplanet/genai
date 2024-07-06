6d9dec9ce33510f61f4371b59719cfe2892347de
sagar-aiplanet <hemasagar@aiplanet.com>
2024-07-05 23:53:20 -0400

    ai assistant

--- Usecases/zml-medical-data/ZML.py
+++ Usecases/zml-medical-data/ZML.py
@@ -54,9 +54,9 @@ if submit:
     retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
     llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})    
     if not uploaded_data_files:
-        system_prompt = "you will act like a chat ...."
+        system_prompt = "You are an AI assistant...."
         pipeline = generator.Generate(question=question, retriever=retriever,system_prompt=system_prompt, llm=llm)
-        st.write("Please upload a file")
+        st.write("Hi, Please upload a file")
     else:
         
         # option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson', 'Leslie Terry','Danny Smith'))

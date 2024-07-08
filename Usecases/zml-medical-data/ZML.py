ec38ab586d4929b9567b9bd7f0360216e8b6a99a
sagar-aiplanet <hemasagar@aiplanet.com>
2024-07-08 18:05:28 -0400

    data_source

--- Usecases/zml-medical-data/ZML.py
+++ Usecases/zml-medical-data/ZML.py
@@ -42,7 +42,6 @@ def uploaded_files(uploaded_data_files):
             filenames.append(file_path)
             with open(file_path, "wb") as f:
                 f.write(file.getbuffer())
-            print("filenames",filenames)
         data = source.fit(filenames, dtype="pdf", chunk_size=1024, chunk_overlap=0)
         # reader = SimpleDirectoryReader(input_dir=filenames[0])
         # documents = reader.load_data()

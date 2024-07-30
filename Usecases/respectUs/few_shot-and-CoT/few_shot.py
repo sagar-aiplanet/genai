442bc6b6eb2d8df463a8d0ac7b360c2b31e0fc0a
sagar-aiplanet <hemasagar@aiplanet.com>
2024-07-30 12:58:31 -0400

    uploaded_files

--- Usecases/respectUs/few_shot-and-CoT/few_shot.py
+++ Usecases/respectUs/few_shot-and-CoT/few_shot.py
@@ -242,13 +242,14 @@ if submit:
     import pydot
     dot_content = answer
     # Create a graph from DOT content
-    (graph,)= pydot.graph_from_dot_data(dot_content)
-    # graph = graphs[0]
+    graph= pydot.graph_from_dot_data(dot_content)
+    graph = graphs[0]
 
     # Save the graph to a PNG file
-    graph.write_png('dot_graph_2.png')
+    graph.write_png('./uploaded_files/dot_graph_2.png')
     # graph.draw('dot_graph_2.png')
     image_name='dot_graph_2.png'
+
     with open('dot_graph_2.png', "rb") as file:
         btn = st.download_button(
                 label="Download image",
